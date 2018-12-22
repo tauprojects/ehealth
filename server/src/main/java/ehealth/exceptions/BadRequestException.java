458850c1fc129390da7f4e0cc7c44e52c9e8e967
Matan Gizunterman <matan.gizunterman@arm.com>
2018-12-22 15:53:25 +0200

    Add BAD_REQUEST  exception

---
+++ server/src/main/java/ehealth/exceptions/BadRequestException.java
@@ -0,0 +1,8 @@
+package ehealth.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.BAD_REQUEST, reason = "Bad username or password")
+public class BadRequestException extends RuntimeException {
+}
