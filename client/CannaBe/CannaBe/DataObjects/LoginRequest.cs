c6533abe3c63ef94ef062771098b5877c17c9eff
Almog Zeltsman <almog6564@gmail.com>
2018-12-22 15:18:33 +0200

    login works

--- client/CannaBe/CannaBe/DataObjects/LoginRequest.cs
+++ client/CannaBe/CannaBe/DataObjects/LoginRequest.cs
@@ -1,11 +1,15 @@
 ﻿using System.Collections.Generic;
 using System.Net.Http;
-
+using System.Text;
+using Newtonsoft.Json;
 namespace CannaBe
 {
     class LoginRequest
     {
+        [JsonProperty("username")]
         string Username { get; set; }
+
+        [JsonProperty("password")]
         string Password { get; set; }
 
         public LoginRequest(string username, string password)
@@ -14,15 +18,17 @@ namespace CannaBe
             this.Password = password;
         }
 
-        public static implicit operator FormUrlEncodedContent(LoginRequest req)
+        public static implicit operator HttpContent(LoginRequest req)
         {
-            var dict = new Dictionary<string, string>
-            {
-                { "username", req.Username },
-                { "password",  req.Password }
-            };
+            //from: https://stackoverflow.com/questions/23585919/send-json-via-post-in-c-sharp-and-receive-the-json-returned
 
-            return new FormUrlEncodedContent(dict);
+            // Serialize our concrete class into a JSON String
+            var stringPayload =  JsonConvert.SerializeObject(req);
+
+            // Wrap our JSON inside a StringContent which then can be used by the HttpClient class
+            var httpContent = new StringContent(stringPayload, Encoding.UTF8, "application/json");
+
+            return httpContent;
         }
     }
 }
