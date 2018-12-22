96a8aa54af33a2e45a3b9287a65479ba6dbb3c9b
Almog Zeltsman <almog6564@gmail.com>
2018-12-23 00:33:29 +0200

    fix exception in get, add Constants

---
+++ client/CannaBe/CannaBe/Utils/Constants.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CannaBe
+{
+    static class Constants
+    {
+        public const string CannaBeUrl = "http://ehealth.westeurope.cloudapp.azure.com:8080/";
+
+        public static string MakeUrl(string addition)
+        {
+            return CannaBeUrl + addition;
+        }
+    }
+}
