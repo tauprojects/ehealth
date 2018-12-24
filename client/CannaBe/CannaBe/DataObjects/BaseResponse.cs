9282df252df43c939a9d3ec9d31a45cd2270c331
Almog Zeltsman <almog6564@gmail.com>
2018-12-24 11:26:54 +0200

    add BaseResponse

---
+++ client/CannaBe/CannaBe/DataObjects/BaseResponse.cs
@@ -0,0 +1,34 @@
+﻿using Newtonsoft.Json;
+using System.Net.Http;
+
+namespace CannaBe
+{
+    class BaseResponse
+    {
+        [JsonProperty("request_id")]
+        string RequestId { get; set; }
+
+        [JsonProperty("status")]
+        string Status { get; set; }
+
+        [JsonProperty("body")]
+        string Body { get; set; }
+
+        public BaseResponse(string requestId, string status, string body)
+        {
+            RequestId = requestId;
+            Status = status;
+            Body = body;
+        }
+
+        public BaseResponse(HttpResponseMessage msg)
+        {
+            var str = msg.Content.ReadAsStringAsync().Result;
+            var response = JsonConvert.DeserializeObject<BaseResponse>(str);
+
+            RequestId = response.RequestId;
+            Status = response.Status;
+            Body = response.Body;
+        }
+    }
+}
