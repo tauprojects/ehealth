da5705009b322f3ab23916e50869d39c485391ec
omerbracha <omerbracha@gmail.com>
2018-12-29 16:55:35 +0200

    commit

---
+++ client/CannaBe/CannaBe/AppPages/InformationPages/MedicalInformationPage.xaml.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace CannaBe.AppPages.InformationPages
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MedicalInformationPage : Page
+    {
+        public MedicalInformationPage()
+        {
+            this.InitializeComponent();
+            this.FixPageSize();
+            PagesUtilities.AddBackButtonHandler();
+        }
+
+        private void BoxGotFocus(object sender, RoutedEventArgs e)
+        {
+            TextBox textBoxSender = sender as TextBox;
+
+            if (textBoxSender.Text == ("Enter " + textBoxSender.Name))
+            {
+                textBoxSender.Text = "";
+                textBoxSender.Foreground = new Windows.UI.Xaml.Media.SolidColorBrush(Windows.UI.Colors.Black);
+            }
+        }
+
+        private void BoxLostFocus(object sender, RoutedEventArgs e)
+        {
+            TextBox textBoxSender = sender as TextBox;
+
+            if (textBoxSender.Text.Length == 0)
+            {
+                textBoxSender.Foreground = new Windows.UI.Xaml.Media.SolidColorBrush(Windows.UI.Colors.Black);
+                textBoxSender.Text = "Enter " + textBoxSender.Name;
+
+            }
+        }
+
+        public void OnPageLoaded(object sender, RoutedEventArgs e)
+        {
+            PagesUtilities.DontFocusOnAnythingOnLoaded(sender, e);
+        }
+
+        private void BackToInformationPage(object sender, TappedRoutedEventArgs e)
+        {
+            Frame.Navigate(typeof(InformationPage));
+        }
+
+        private void GoToDashboard(object sender, TappedRoutedEventArgs e)
+        {
+            Frame.Navigate(typeof(DashboardPage));
+        }
+    }
+}
