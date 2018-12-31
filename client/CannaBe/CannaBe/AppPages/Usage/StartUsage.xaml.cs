1ed36b4c5a4a61b180c8091b50990871f38d0eae
Almog Zeltsman <almog6564@gmail.com>
2019-01-01 01:52:35 +0200

    start usage, strain object

---
+++ client/CannaBe/CannaBe/AppPages/Usage/StartUsage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System.Net.Http;
+using Windows.UI.Xaml.Controls;
+
+namespace CannaBe.AppPages.Usage
+{
+    public sealed partial class StartUsage : Page
+    {
+        public StartUsage()
+        {
+            this.InitializeComponent();
+        }
+
+        private void LoadStrainList(object sender, object e)
+        {
+            var url = Constants.MakeUrl("ehealth/strain/all");
+            //try
+            //{
+            //    var res = HttpManager.Manager.Get(url);
+
+            //    var str = await res.Result.Content.ReadAsStringAsync();
+
+            //    AppDebug.Line(str);
+            //    await new MessageDialog(str, "Search Strain").ShowAsync();
+            //}
+            //catch (Exception ex)
+            //{
+            //    AppDebug.Exception(ex, "SearchStrain");
+            //    await new MessageDialog("Failed get: \n" + url, "Exception in Search Strain").ShowAsync();
+            //}
+        }
+    }
+}
---
+++ client/CannaBe/CannaBe/DataObjects/Strain.cs
@@ -0,0 +1,33 @@
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
+
+namespace CannaBe
+{
+    class Strain
+    {
+        [JsonProperty("name")]
+        public string Name;
+
+        [JsonProperty("id")]
+        public int ID;
+
+        [JsonProperty("medical")]
+        public List<string> MedicalNeeds { get; set; }
+
+        [JsonProperty("positive")]
+        public List<string> PositivePreferences { get; set; }
+
+        [JsonProperty("negative")]
+        public List<string> NegativePreferences { get; set; }
+
+        [JsonConstructor]
+        public Strain(string name, int iD, List<string> medicalNeeds, List<string> positivePreferences, List<string> negativePreferences)
+        {
+            Name = name;
+            ID = iD;
+            MedicalNeeds = medicalNeeds;
+            PositivePreferences = positivePreferences;
+            NegativePreferences = negativePreferences;
+        }
+    }
+}
